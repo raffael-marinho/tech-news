369718db4e70ae5bf97ee18028a9cb695b349f8c
raffael-marinho <silvamarinho10@gmail.com>
2022-10-03 22:58:24 -0300

    req 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -35,7 +35,33 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = parsel.Selector(html_content)
+    url = selector.css('head link[rel=canonical]::attr(href)').get()
+    title = selector.css("h1.entry-title::text").get().strip()
+    timestamp = selector.css("li.meta-date::text").get()
+    writer = selector.css(".author a::text").get()
+
+    comments_count = selector.css(".post-comments::text").get()
+    if comments_count is None:
+        comments_count = 0
+
+    summary = selector.css(
+        "div.entry-content > p:nth-of-type(1) *::text").getall()
+
+    tags = selector.css("a[rel=tag]::text").getall()
+    category = selector.css(".label::text").get()
+
+    news = {
+            "url": url,
+            "title": title,
+            "timestamp": timestamp,
+            "writer": writer,
+            "comments_count": comments_count,
+            "summary": ''.join(summary).strip(),
+            "tags": tags,
+            "category": category,
+            }
+    return news
 
 
 # Requisito 5
