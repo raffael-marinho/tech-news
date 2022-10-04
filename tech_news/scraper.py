69e75553c3c7b927c09901f05ba1d90a1aa19a3c
raffael-marinho <silvamarinho10@gmail.com>
2022-10-03 22:37:43 -0300

    req 3

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import requests
 from time import sleep
+import parsel
 
 
 # Requisito 1
@@ -17,7 +18,9 @@ def fetch(url):
 
 # Requisito 2
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = parsel.Selector(text=html_content)
+    links = selector.css(".cs-overlay-link ::attr(href)").getall()
+    return links
 
 
 # Requisito 3
