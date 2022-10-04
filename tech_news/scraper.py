9932fbf9763d51fa124f274c1cebad0da8571cda
raffael-marinho <silvamarinho10@gmail.com>
2022-10-03 22:25:57 -0300

    req 1

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,23 +1,35 @@
+import requests
+from time import sleep
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        sleep(1)
+        response = requests.get(
+            url, timeout=3, headers={"user-agent": "Fake user-agent"})
+        response.raise_for_status()
+    except (requests.HTTPError, requests.ReadTimeout):
+        return
+    else:
+        return response.text
 
 
 # Requisito 2
-def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+""" def scrape_novidades(html_content):
+ """    """Seu código deve vir aqui"""
 
 
 # Requisito 3
-def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+""" def scrape_next_page_link(html_content):
+ """    """Seu código deve vir aqui"""
 
 
 # Requisito 4
-def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+""" def scrape_noticia(html_content):
+ """    """Seu código deve vir aqui"""
 
 
 # Requisito 5
-def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+""" def get_tech_news(amount):
+ """    """Seu código deve vir aqui"""
